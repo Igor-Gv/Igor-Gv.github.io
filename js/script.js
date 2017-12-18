b9966f55a88f594f4ce459bd5155d6349cd3519a
Igor-Gv <34650140+Igor-Gv@users.noreply.github.com>
2017-12-18 17:55:55 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+      $('.slider-tel').slick(
+{
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2500,
+  fade: true,
+}
+      	);
+
+ });
